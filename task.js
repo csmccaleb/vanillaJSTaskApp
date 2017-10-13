fa61b9e84dce4d54c2c299e923a876daeb705b2b
cmccaleb <cs.mccaleb@gmail.com>
2017-10-13 17:14:21 -0500

    reorganizing code, adding exports

--- task.js
+++ task.js
@@ -21,12 +21,4 @@ Task.prototype.save  = function() {
   console.log('saving Task: ' + this.name);
 };
 
-var task1 = new Task('create a demo for constructors');
-var task2 = new Task('create a demo for modules');
-var task3 = new Task('create a demo for singletons');
-var task4 = new Task('create a demo for prototypes');
-
-task1.complete();
-task2.save();
-task3.save();
-task4.save();
+module.exports = Task;
